71eda1ea70873e239dff954e8fa5d663ae8269be
pete <peter.d.alexander23@gmail.com>
2022-12-22 19:00:07 +0000

    Add complete day 3

---
+++ src/day3_rucksack_reorganization/mod.rs
@@ -0,0 +1,48 @@
+use std::char;
+
+pub fn process_data(data_lines: Vec<String>) -> (i32, i32) {
+    // Declare a vector to hold the shared item in each line
+    let mut shared_char_vec = vec!['@'; data_lines.len()];
+
+    // Split the lines down the middle
+    let split_data_lines: Vec<(&str, &str)> =
+        data_lines.iter().map(|l| l.split_at(l.len() / 2)).collect();
+
+    // Save the line the characters have in common
+    for (i, (line1, line2)) in split_data_lines.iter().enumerate() {
+        for char in line1.chars() {
+            if line2.contains(char) {
+                shared_char_vec[i] = char;
+            }
+        }
+    }
+
+    // Convert the characters into their numerical values counting up from a = 1
+    let mut part1_score_sum: i32 = 0;
+    for c in shared_char_vec {
+        part1_score_sum += get_char_priority(c);
+    }
+
+    // Part 2
+    // Split the elves into their groups of three
+    let part2_score_sum: i32 = data_lines
+        .chunks(3)
+        .map(|g| {
+            for c in g[0].chars() {
+                if g[1].contains(c) && g[2].contains(c) {
+                    return get_char_priority(c);
+                }
+            }
+            panic!("Found a group with no common character!")
+        })
+        .sum();
+    (part1_score_sum, part2_score_sum)
+}
+
+fn get_char_priority(c: char) -> i32 {
+    if c.is_uppercase() {
+        (c as u8 - 38) as i32
+    } else {
+        (c as u8 - 96) as i32
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use advent_of_code_2022::run_puzzle_script;
 
 mod day1_calorie_counting;
 mod day2_rock_paper_scissors;
+mod day3_rucksack_reorganization;
 
 fn main() {
     run_puzzle_script(
@@ -9,9 +10,16 @@ fn main() {
         (24000, 41000),
         "day1_calorie_counting".to_string(),
     );
+
     run_puzzle_script(
         day2_rock_paper_scissors::process_data,
         (15, 12),
         "day2_rock_paper_scissors".to_string(),
+    );
+
+    run_puzzle_script(
+        day3_rucksack_reorganization::process_data,
+        (157, 70),
+        "day3_rucksack_reorganization".to_string(),
     );
 }
