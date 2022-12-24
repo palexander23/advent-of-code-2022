e95229c928c1e611ae403ac6b7a639b0d01919d5
pete <peter.d.alexander23@gmail.com>
2022-12-24 09:57:49 +0000

    Add complete day 4

--- src/day4_camp_cleanup/mod.rs
+++ src/day4_camp_cleanup/mod.rs
@@ -1,3 +1,4 @@
+use lazy_static::lazy_static;
 use regex::Regex;
 
 pub fn process_data(data_lines: Vec<String>) -> (i32, i32) {
@@ -8,6 +9,16 @@ pub fn process_data(data_lines: Vec<String>) -> (i32, i32) {
         .collect();
 
     // Sum up the number of pairs that overlap
+    let contains_sum: i32 = ranges_vec
+        .iter()
+        .map(|r| {
+            let ((l1, h1), (l2, h2)) = r;
+            check_contains(l1, h1, l2, h2)
+        })
+        .map(|b| b as i32)
+        .sum();
+
+    // Part Two
     let overlaps_sum: i32 = ranges_vec
         .iter()
         .map(|r| {
@@ -17,13 +28,15 @@ pub fn process_data(data_lines: Vec<String>) -> (i32, i32) {
         .map(|b| b as i32)
         .sum();
 
-    (overlaps_sum, 0)
+    (contains_sum, overlaps_sum)
 }
 
 fn get_int_ranges_from_input_line(line: &str) -> ((i32, i32), (i32, i32)) {
-    let range_regex = Regex::new(r"(\d*)-(\d*),(\d*)-(\d*)").unwrap();
+    lazy_static! {
+        static ref RANGE_REGEX: Regex = Regex::new(r"(\d*)-(\d*),(\d*)-(\d*)").unwrap();
+    }
 
-    let caps = range_regex.captures(&line).unwrap();
+    let caps = RANGE_REGEX.captures(&line).unwrap();
     (
         (
             caps.get(1).unwrap().as_str().parse().unwrap(),
@@ -36,7 +49,7 @@ fn get_int_ranges_from_input_line(line: &str) -> ((i32, i32), (i32, i32)) {
     )
 }
 
-fn check_overlap(l1: &i32, h1: &i32, l2: &i32, h2: &i32) -> bool {
+fn check_contains(l1: &i32, h1: &i32, l2: &i32, h2: &i32) -> bool {
     // Check if l2-h2 is inside l1-h1
     if l2 >= l1 && h2 <= h1 {
         return true;
@@ -44,6 +57,15 @@ fn check_overlap(l1: &i32, h1: &i32, l2: &i32, h2: &i32) -> bool {
 
     // Check if l1-h1 is inside l2-h2
     if l1 >= l2 && h1 <= h2 {
+        return true;
+    }
+
+    return false;
+}
+
+fn check_overlap(l1: &i32, h1: &i32, l2: &i32, h2: &i32) -> bool {
+    // Check inner overlap
+    if l2 <= h1 && h2 >= l1 {
         return true;
     }
 
